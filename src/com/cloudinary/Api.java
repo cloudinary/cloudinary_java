204db8e5ee8f316ad67218f9d793f5cec1ece369
Tal Lev-Ami <tal.levami@gmail.com>
2013-02-08 15:28:20 +0200

    Support image_metadata flag in upload and API

--- src/com/cloudinary/Api.java
+++ src/com/cloudinary/Api.java
@@ -127,7 +127,7 @@ public class Api {
 		String resourceType = Cloudinary.asString(options.get("resource_type"), "image");
 		String type = Cloudinary.asString(options.get("type"), "upload");
 		return callApi(HttpMethod.GET, Arrays.asList("resources", resourceType, type, public_id),
-				only(options, "exif", "colors", "faces", "pages", "max_results"), options);
+				only(options, "exif", "colors", "faces", "image_metadata", "pages", "max_results"), options);
 	}
 
 	public Map deleteResources(Iterable<String> publicIds, Map options) throws Exception {
--- src/com/cloudinary/Uploader.java
+++ src/com/cloudinary/Uploader.java
@@ -31,7 +31,7 @@ public class Uploader {
 	public Uploader(Cloudinary cloudinary) {
 		this.cloudinary = cloudinary;
 	}
-	static final String[] BOOLEAN_UPLOAD_OPTIONS = new String[] {"backup", "exif", "faces", "colors"};
+	static final String[] BOOLEAN_UPLOAD_OPTIONS = new String[] {"backup", "exif", "faces", "colors", "image_metadata"};
 
 	public Map<String, String> buildUploadParams(Map options) {
         if (options == null) options = Cloudinary.emptyMap();

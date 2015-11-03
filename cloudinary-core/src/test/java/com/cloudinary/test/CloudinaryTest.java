78c5a929dce3c02d42497060a4998c167de629a0
Amir Tocker <tocker.signup@gmail.com>
2015-11-03 17:20:17 +0200

    Create separate test class for Layer

--- cloudinary-core/src/test/java/com/cloudinary/test/CloudinaryTest.java
+++ cloudinary-core/src/test/java/com/cloudinary/test/CloudinaryTest.java
@@ -255,37 +255,6 @@ public class CloudinaryTest {
 	}
 
 	@Test
-	public void testOverlay() {
-		// should support overlay
-		Transformation transformation = new Transformation().overlay("text:hello");
-		String result = cloudinary.url().transformation(transformation).generate("test");
-		assertEquals(DEFAULT_UPLOAD_PATH + "l_text:hello/test", result);
-		// should not pass width/height to html if overlay
-		transformation = new Transformation().overlay("text:hello").width(100).height(100);
-		result = cloudinary.url().transformation(transformation).generate("test");
-		assertNull(transformation.getHtmlHeight());
-		assertNull(transformation.getHtmlWidth());
-		assertEquals(DEFAULT_UPLOAD_PATH + "h_100,l_text:hello,w_100/test", result);
-		
-		transformation = new Transformation().overlay(new TextLayer().text("goodbye"));
-		result = cloudinary.url().transformation(transformation).generate("test");
-		assertEquals(DEFAULT_UPLOAD_PATH + "l_text:goodbye/test", result);
-	}
-
-	@Test
-	public void testUnderlay() {
-		Transformation transformation = new Transformation().underlay("text:hello");
-		String result = cloudinary.url().transformation(transformation).generate("test");
-		assertEquals(DEFAULT_UPLOAD_PATH + "u_text:hello/test", result);
-		// should not pass width/height to html if underlay
-		transformation = new Transformation().underlay("text:hello").width(100).height(100);
-		result = cloudinary.url().transformation(transformation).generate("test");
-		assertNull(transformation.getHtmlHeight());
-		assertNull(transformation.getHtmlWidth());
-		assertEquals(DEFAULT_UPLOAD_PATH + "h_100,u_text:hello,w_100/test", result);
-	}
-
-	@Test
 	public void testFetchFormat() {
 		// should support format for fetch urls
 		String result = cloudinary.url().format("jpg").type("fetch").generate("http://cloudinary.com/images/old_logo.png");
@@ -899,47 +868,6 @@ public class CloudinaryTest {
 		actual = cloudinary.url().transformation(new Transformation().aspectRatio(3,2))
 				.generate("test");
 		assertEquals(DEFAULT_UPLOAD_PATH + "ar_3:2/test", actual);
-	}
-
-	@Test
-	public void testOverlayOptions() {
-		Object tests[] = {
-				new Layer().publicId("logo"),
-				"logo",
-				new Layer().publicId("folder/logo"),
-				"folder:logo",
-				new Layer().publicId("logo").type("private"),
-				"private:logo",
-				new Layer().publicId("logo").format("png"),
-				"logo.png",
-				new Layer().resourceType("video").publicId("cat"),
-				"video:cat",
-				new TextLayer().text("Hello World, Nice to meet you?").fontFamily("Arial").fontSize(18),
-				"text:Arial_18:Hello%20World%E2%80%9A%20Nice%20to%20meet%20you%3F",
-				new TextLayer().text("Hello World, Nice to meet you?").fontFamily("Arial").fontSize(18)
-						.fontWeight("bold").fontStyle("italic").letterSpacing("4"),
-				"text:Arial_18_bold_italic_letter_spacing_4:Hello%20World%E2%80%9A%20Nice%20to%20meet%20you%3F",
-				new SubtitlesLayer().publicId("sample_sub_en.srt"), "subtitles:sample_sub_en.srt",
-				new SubtitlesLayer().publicId("sample_sub_he.srt").fontFamily("Arial").fontSize(40),
-				"subtitles:Arial_40:sample_sub_he.srt" };
-
-		for (int i = 0; i < tests.length; i += 2) {
-			Object layer = tests[i];
-			String expected = (String) tests[i + 1];
-			assertEquals(expected, layer.toString());
-		}
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testOverlayError1() {
-		// Must supply font_family for text in overlay
-		cloudinary.url().transformation(new Transformation().overlay(new TextLayer().fontStyle("italic"))).generate("test");
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testOverlayError2() {
-		// Must supply public_id for for non-text underlay
-		cloudinary.url().transformation(new Transformation().underlay(new Layer().resourceType("video"))).generate("test");
 	}
 
 	public static Map<String, String> getUrlParameters(URI uri) throws UnsupportedEncodingException {

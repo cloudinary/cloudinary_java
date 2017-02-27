db4866b1ce43392e877e9f0d80171ade5b286dab
Nitzan Jaitman <nitzan@cloudinary.com>
2017-02-27 12:56:31 +0200

    Add test for moderation param in explicit call

--- cloudinary-test-common/src/main/java/com/cloudinary/test/AbstractUploaderTest.java
+++ cloudinary-test-common/src/main/java/com/cloudinary/test/AbstractUploaderTest.java
@@ -155,7 +155,7 @@ abstract public class AbstractUploaderTest extends MockableTest {
 
     @Test
     public void testExplicit() throws IOException {
-        Map result = cloudinary.uploader().explicit("sample", asMap("eager", Collections.singletonList(new Transformation().crop("scale").width(2.0)), "type", "upload"));
+        Map result = cloudinary.uploader().explicit("sample", asMap("eager", Collections.singletonList(new Transformation().crop("scale").width(2.0)), "type", "upload", "moderation", "manual"));
         String url = cloudinary.url().transformation(new Transformation().crop("scale").width(2.0)).format("jpg").version(result.get("version")).generate("sample");
         String eagerUrl = (String) ((Map) ((List) result.get("eager")).get(0)).get("url");
         String cloudName = cloudinary.config.cloudName;

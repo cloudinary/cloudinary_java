c8d370b64225796a40a38b4909d4a1eab62aebd8
Nitzan Jaitman <nitzanj@users.noreply.github.com>
2017-03-08 13:55:41 +0200

    Fix encoding error in api update resource (#61)

--- cloudinary-http43/src/main/java/com/cloudinary/http43/ApiStrategy.java
+++ cloudinary-http43/src/main/java/com/cloudinary/http43/ApiStrategy.java
@@ -9,6 +9,7 @@ import com.cloudinary.http43.api.Response;
 import com.cloudinary.utils.Base64Coder;
 import com.cloudinary.utils.ObjectUtils;
 import com.cloudinary.utils.StringUtils;
+import org.apache.http.Consts;
 import org.apache.http.HttpHost;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
@@ -159,7 +160,7 @@ public class ApiStrategy extends com.cloudinary.strategies.AbstractApiStrategy {
                 default:
                     throw new IllegalArgumentException("Unknown HTTP method");
             }
-            ((HttpEntityEnclosingRequestBase) request).setEntity(new UrlEncodedFormEntity(parameters));
+            ((HttpEntityEnclosingRequestBase) request).setEntity(new UrlEncodedFormEntity(parameters, Consts.UTF_8));
         }
 
         setTimeouts(request, options);

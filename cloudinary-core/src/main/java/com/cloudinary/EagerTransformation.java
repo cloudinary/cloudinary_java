a9e8890cc5780608a698424c13d3df76bb6338d8
Nitzan Jaitman <nitzan@cloudinary.com>
2017-04-30 12:04:25 +0300

    Add generics to `Transformation` class.
    Allows for chaining commands when working with derived classes and calling a super method.

--- cloudinary-core/src/main/java/com/cloudinary/EagerTransformation.java
+++ cloudinary-core/src/main/java/com/cloudinary/EagerTransformation.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class EagerTransformation extends Transformation {
+public class EagerTransformation extends Transformation<EagerTransformation> {
     protected String format;
 
     @SuppressWarnings("rawtypes")

fe17b050e467702e59eaabc273d8f671991e71b5
Nitzan Jaitman <nitzan@cloudinary.com>
2017-08-22 11:02:34 +0300

    Fix boolean config values in tag generation

--- cloudinary-taglib/src/main/java/com/cloudinary/taglib/CloudinaryJsConfigTag.java
+++ cloudinary-taglib/src/main/java/com/cloudinary/taglib/CloudinaryJsConfigTag.java
@@ -9,8 +9,8 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.Singleton;
 
-public class CloudinaryJsConfigTag  extends SimpleTagSupport {
-	@SuppressWarnings("unused")
+public class CloudinaryJsConfigTag extends SimpleTagSupport {
+    @SuppressWarnings("unused")
     public void doTag() throws JspException, IOException {
         Cloudinary cloudinary = Singleton.getCloudinary();
         if (cloudinary == null) {
@@ -18,23 +18,20 @@ public class CloudinaryJsConfigTag  extends SimpleTagSupport {
         }
         JspWriter out = getJspContext().getOut();
         out.println("<script language='javascript' type='text/javascript'>$.cloudinary.config({");
-        String[] keys = {"api_key", "cloud_name", "cdn_subdomain"};
-		String[] boolKeys = {"private_cdn", "secure_distribution"};
-        
-        print(out,"api_key",cloudinary.config.apiKey);
-        print(out,"cloud_name",cloudinary.config.cloudName);
-        print(out,"cdn_subdomain",cloudinary.config.cdnSubdomain);
-        
-        print(out,"private_cdn",cloudinary.config.privateCdn);
-        print(out,"secure_distribution",cloudinary.config.secureDistribution);
-        
-        
-       
+        print(out, "api_key", cloudinary.config.apiKey);
+        print(out, "cloud_name", cloudinary.config.cloudName);
+        print(out, "cdn_subdomain", cloudinary.config.cdnSubdomain);
+        print(out, "private_cdn", cloudinary.config.privateCdn);
+        print(out, "secure_distribution", cloudinary.config.secureDistribution);
+
         out.println("});</script>");
     }
 
-	private void print(JspWriter out, String key,Object value) throws IOException {
-		out.println(key + ": \""+value + "\",");
-		
-	}
+    private void print(JspWriter out, String key, Object value) throws IOException {
+        if (value instanceof Boolean) {
+            out.println(key + ": " + ((Boolean) value ? "true" : "false") + ",");
+        } else {
+            out.println(key + ": \"" + value + "\",");
+        }
+    }
 }

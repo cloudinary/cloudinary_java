5b1eb03d6c5f3afa50f37f3ed5efd3bfe19a5136
Nitzan Jaitman <nitzan@cloudinary.com>
2018-08-08 16:58:28 +0300

    Fix bad token in javadoc.

--- cloudinary-core/src/main/java/com/cloudinary/utils/StringUtils.java
+++ cloudinary-core/src/main/java/com/cloudinary/utils/StringUtils.java
@@ -244,7 +244,7 @@ public class StringUtils {
     }
 
     /**
-     * Merge all consecutive underscores and spaces into a single underscore, e.g. "ab___c_  _d" -> "ab_c_d"
+     * Merge all consecutive underscores and spaces into a single underscore, e.g. "ab___c_  _d" becomes "ab_c_d"
      *
      * @param s String to process
      * @return The resulting string.
@@ -363,7 +363,7 @@ public class StringUtils {
     }
 
     /**
-     * Merges all occurrences of multiple slashes into a single slash (e.g. "a///b//c/d" -> "a/b/c/d")
+     * Merges all occurrences of multiple slashes into a single slash (e.g. "a///b//c/d" becomes "a/b/c/d")
      * @param url The string to process
      * @return The resulting string with merged slashes.
      */

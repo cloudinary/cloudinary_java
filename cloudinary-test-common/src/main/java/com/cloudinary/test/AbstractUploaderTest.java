66cff26d16298e0a19114d0078869e64ed8d92bf
adimiz1 <adi.mizrahi@cloudinary.com>
2022-09-01 14:19:27 +0300

    Add use_asset_folder_as_public_id_prefix

--- cloudinary-test-common/src/main/java/com/cloudinary/test/AbstractUploaderTest.java
+++ cloudinary-test-common/src/main/java/com/cloudinary/test/AbstractUploaderTest.java
@@ -801,16 +801,18 @@ abstract public class AbstractUploaderTest extends MockableTest {
 
     @Test
     public void testUploadFolderDecoupling() {
-        //TODO: Need to build a unit testing infrastructure
         Map options = asMap(
                 "use_filename_as_display_name", true,
                 "public_id_prefix", "test_id_prefix",
                 "asset_folder", "asset_folder_test",
-                "display_name", "display_name_test");
+                "display_name", "display_name_test",
+                "use_asset_folder_as_public_id_prefix", true);
+
         Map uploadParams = Util.buildUploadParams(options);
         Assert.assertEquals("test_id_prefix", uploadParams.get("public_id_prefix"));
         Assert.assertEquals(true, uploadParams.get("use_filename_as_display_name"));
         Assert.assertEquals("asset_folder_test", uploadParams.get("asset_folder"));
         Assert.assertEquals("display_name_test", uploadParams.get("display_name"));
+        Assert.assertEquals(true, uploadParams.get("use_asset_folder_as_public_id_prefix"));
     }
 }

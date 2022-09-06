de5e743805cb5350ae5e028a9d5f1ee6d3aaf072
adimiz1 <adi.mizrahi@cloudinary.com>
2022-09-06 09:04:46 +0300

    Fix auth test

--- cloudinary-core/src/test/java/com/cloudinary/AuthTokenTest.java
+++ cloudinary-core/src/test/java/com/cloudinary/AuthTokenTest.java
@@ -121,6 +121,7 @@ public class AuthTokenTest {
         String message = "should add token to an image tag url";
         String url = cloudinary.url().signed(true).resourceType("image").type("authenticated").version("1486020273").imageTag("sample.jpg");
         assertThat(url, Matchers.matchesPattern("<img.*src='http://res.cloudinary.com/test123/image/authenticated/v1486020273/sample.jpg\\?__cld_token__=st=11111111~exp=11111411~hmac=9bd6f41e2a5893da8343dc8eb648de8bf73771993a6d1457d49851250caf3b80.*>"));
+
     }
 
     @Test

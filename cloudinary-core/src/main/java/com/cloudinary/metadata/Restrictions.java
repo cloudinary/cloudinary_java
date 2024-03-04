cdd1b912ac59801b8bf78e1002f04e6af418e8a0
adimiz1 <adi.mizrahi@cloudinary.com>
2024-03-04 15:16:45 +0200

    Add doc strings

--- cloudinary-core/src/main/java/com/cloudinary/metadata/Restrictions.java
+++ cloudinary-core/src/main/java/com/cloudinary/metadata/Restrictions.java
@@ -2,19 +2,34 @@ package com.cloudinary.metadata;
 
 import java.util.HashMap;
 
+/**
+ * Represents the restrictions metadata field.
+ */
 public class Restrictions {
 
     HashMap restrictions = new HashMap();
 
+    /**
+     * Set the custom field into restrictions.
+     * @param key The key of the field.
+     * @param value The value of the field.
+     */
     public Restrictions setRestriction(String key, Object value) {
         restrictions.put(key, value);
         return this;
     }
 
+    /**
+     * Set the read only ui field.
+     * @param value The read only ui value.
+     */
     public Restrictions setReadOnlyUI(Boolean value) {
         return setRestriction("readonly_ui", value);
     }
 
+    /**
+     * Set the read only ui field to true.
+     */
     public Restrictions setReadOnlyUI() {
         return this.setReadOnlyUI(true);
     }

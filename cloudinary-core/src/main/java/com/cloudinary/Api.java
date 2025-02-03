e0aec62b06d0d8bd9159d406629853096758f10d
mizadi <adi.mizrahi@gmail.com>
2025-02-03 14:12:06 +0200

    Fix api call

--- cloudinary-core/src/main/java/com/cloudinary/Api.java
+++ cloudinary-core/src/main/java/com/cloudinary/Api.java
@@ -660,6 +660,7 @@ public class Api {
      * @throws Exception When the folder isn't empty or doesn't exist.
      */
     public ApiResponse deleteFolder(String folder, Map options) throws Exception {
+        if (options == null || options.isEmpty()) options = ObjectUtils.asMap();
         List<String> uri = Arrays.asList("folders", folder);
         Map params = ObjectUtils.only(options, "skip_backup");
         return callApi(HttpMethod.DELETE, uri, params, options);
